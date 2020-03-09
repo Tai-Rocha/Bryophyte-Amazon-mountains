425b1f5d9b8870ddaa9ce31b33661f23d1d00bb9
Tai-Rocha <taina013@gmail.com>
2020-03-09 14:48:37 -0300

    Correlation 21 variable from wordclim

---
+++ Correlation_updated.R
@@ -0,0 +1,30 @@
+#################################################################
+# Correlation matrix among 21 variables from wordclim version 2  
+# Tainá Rocha                     
+# Last update : 09/03/2020
+#################################################################
+
+########
+# Correlation matrix
+correlation <- round(cor(pca_briofitas), 2)
+correlation
+
+
+# Plot
+
+ggcorrplot(correlation, hc.order = TRUE, 
+           type = "lower", 
+           lab = TRUE, 
+           lab_size = 1.5, 
+           method="circle", 
+           colors = c("tomato2", "white", "springgreen3"), 
+           title=" ", 
+           ggtheme=theme_classic,
+           legend.title="Correlation"
+)
+
+
+ggsave("correlation.tiff", units="in", width=5, height=4, dpi=300, compression = 'lzw')
+
+
+
